e4a7b146f9460b2a0dc3cde3f99d5c565cfe03b8
Tom <yj889200@gmail.com>
2014-08-01 11:05:53 +1000

    auto mac sh updated

--- autorun_mac.sh
+++ autorun_mac.sh
@@ -4,10 +4,12 @@ filename="./tag_num.txt"
 ./clean.sh
 cat $filename | while read LINE
 do
+	mkdir long short
 	make
 	./run $LINE
 	python dir_operation.py
 	mv tmp/replay/*.csv results/${LINE}_flipGFcetd_gf1.csv
+	rm -rf long short
 	./clean.sh
 done 
 
